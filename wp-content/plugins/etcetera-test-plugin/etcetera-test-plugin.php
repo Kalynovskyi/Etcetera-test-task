7fce3cc2d174d2cc029feae8ac13d8af00ec786a
Kalinovsky Nikita <nkalinovsky@gmail.com>
2024-09-23 10:10:24 +0300

    plugin with custom post type and txonomy created

---
+++ wp-content/plugins/etcetera-test-plugin/etcetera-test-plugin.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Plugin Name: Etcetera test plugin
+ * Description: Test plugin.
+ * Version: 1.0
+ */
+
+// Plugin registration
+
+function etcetera_test_real_estate_custom_post_type()
+{
+    $labels = array(
+        'name' => 'Estate Name',
+        'singular_name' => 'Estate Name',
+        'menu_name' => 'Estate Name',
+        'name_admin_bar' => 'Estate Name',
+        'add_new' => 'Add new',
+        'add_new_item' => 'Add new estate',
+        'new_item' => 'New estate',
+        'edit_item' => 'Edit estate',
+        'view_item' => 'View estate',
+        'all_items' => 'All estate',
+        'search_items' => 'Search estate',
+        'not_found' => 'Not found',
+        'not_found_in_trash' => 'Not found in bin',
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array('slug' => 'real-estate'),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => true,
+        'menu_position'      => 20,
+        'supports'           => array('title', 'editor', 'thumbnail', 'excerpt', 'comments'),
+    );
+
+    register_post_type('real_estate', $args);
+}
+
+add_action('init', 'etcetera_test_real_estate_custom_post_type');
+
+
+function etcetera_test_real_estate_register_taxonomy()
+{
+    $labels = array(
+        'name'              => 'District',
+        'singular_name'     => 'District',
+        'search_items'      => 'Search district',
+        'all_items'         => 'All districts',
+        'parent_item'       => 'Parent district',
+        'parent_item_colon' => 'Parent district:',
+        'edit_item'         => 'Edit district',
+        'update_item'       => 'Update district',
+        'add_new_item'      => 'Add new district',
+        'new_item_name'     => 'New district name',
+        'menu_name'         => 'Districts',
+    );
+
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array('slug' => 'district'),
+    );
+
+    register_taxonomy('district', array('real_estate'), $args);
+}
+add_action('init', 'etcetera_test_real_estate_register_taxonomy');
